bfeb1b72359102ba27168c0be41a3eda8fde02c2
snehalkr <30932507+snehalkr@users.noreply.github.com>
2019-03-11 01:10:36 +0530

    Change Indentation

--- helper.js
+++ helper.js
@@ -4,88 +4,88 @@ const path = require('path')
 const app = express()
 
 function getDirList(folder, path) {
-  var arr=[];
-  fullPath = folder + '' + path;
+	var arr=[];
+	fullPath = folder + '' + path;
 
-  if (!fs.existsSync(fullPath)) {
-    return arr;
-  }
+	if (!fs.existsSync(fullPath)) {
+		return arr;
+	}
 
-  fs.readdirSync(fullPath).forEach(file => {
-    var path_string = fullPath+'/'+file;
-    var link = file;
-    var type;
-    if(fs.lstatSync(path_string).isDirectory())
-      type = 0;
-    else
-      type = 1;
-    var element = {link:link, type:type};
-    arr.push(element);
-  });
-  return arr;
+	fs.readdirSync(fullPath).forEach(file => {
+		var path_string = fullPath+'/'+file;
+		var link = file;
+		var type;
+		if(fs.lstatSync(path_string).isDirectory())
+			type = 0;
+		else
+			type = 1;
+		var element = {link:link, type:type};
+		arr.push(element);
+	});
+	return arr;
 }
 
 exports.getDirListJSON = function(folder, path) {
-  result = getDirList(folder, path);
-  curDir = folder + '' + path;
-  jsonRespone = {cur:path,files:result};
-  return jsonRespone;
+	result = getDirList(folder, path);
+	curDir = folder + '' + path;
+	jsonRespone = {cur:path,files:result};
+	return jsonRespone;
 }
 
 
 exports.getDirListHTML = function(folder, path) {
-  var out = '';
+	var out = '';
 
-  result = getDirList(folder, path);
-  result.forEach(function(curItem) {
-    link = path + '/' + curItem.link;
-    if(curItem.type==0)
-      out+='<a href="/no-js?id='+link+'">' + curItem.link + '</a> : Folder</br>\n';
-    else
-      out+='<a href="/video?id='+link+'">' + curItem.link + '</a> : File</br>\n';
-  })
-  return out;
+	result = getDirList(folder, path);
+	result.forEach(function(curItem) {
+		link = path + '/' + curItem.link;
+		if(curItem.type==0)
+			out+='<a href="/no-js?id='+link+'">' + curItem.link + '</a> : Folder</br>\n';
+		else
+			out+='<a href="/video?id='+link+'">' + curItem.link + '</a> : File</br>\n';
+	})
+	return out;
 }
 
 exports.loadVideo = function(folder, req, res) {
-  var path = folder+''+req.query.id;
+	var path = folder+''+req.query.id;
 
-  if (!fs.existsSync(path)) {
-    res.send('Video Not Found!');
-    return ;
-  }
+	if (!fs.existsSync(path)) {
+		res.send('Video Not Found!');
+		return ;
+	}
 
-  const stat = fs.statSync(path)
-  const fileSize = stat.size
-  const range = req.headers.range
+	const stat = fs.statSync(path)
+	const fileSize = stat.size
+	const range = req.headers.range
 
-  var id = req.query.id;
+	var id = req.query.id;
 
-  if (range) {
-    const parts = range.replace(/bytes=/, "").split("-")
-    const start = parseInt(parts[0], 10)
-    const end = parts[1]
-      ? parseInt(parts[1], 10)
-      : fileSize-1
+	if (range) {
+		const parts = range.replace(/bytes=/, "").split("-")
+		const start = parseInt(parts[0], 10)
+		const end = parts[1]
+			? parseInt(parts[1], 10)
+			: fileSize-1
 
-    const chunksize = (end-start)+1
-    const file = fs.createReadStream(path, {start, end})
-    const head = {
-      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': 'video/mp4',
-    }
+		const chunksize = (end-start)+1
+		const file = fs.createReadStream(path, {start, end})
+		const head = {
+			'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+			'Accept-Ranges': 'bytes',
+			'Content-Length': chunksize,
+			'Content-Type': 'video/mp4',
+		}
 
-    res.writeHead(206, head)
-    file.pipe(res)
-  } else {
-    const head = {
-      'Content-Length': fileSize,
-      'Content-Type': 'video/mp4',
-    }
-    res.writeHead(200, head)
-    fs.createReadStream(path).pipe(res)
-  }
+		res.writeHead(206, head)
+		file.pipe(res)
+	} else {
+		const head = {
+			'Content-Length': fileSize,
+			'Content-Type': 'video/mp4',
+		}
+		res.writeHead(200, head)
+		fs.createReadStream(path).pipe(res)
+	}
 }
 // console.log(this.getDirList('C:/unity/nodejs/video-stream-sample'));
--- server.js
+++ server.js
@@ -5,50 +5,48 @@ const app = express()
 const helper = require('./helper')
 
 function logUser(req) {
-  console.log(req.connection.remoteAddress);
+	console.log(req.connection.remoteAddress);
 }
 
 function getPath(req) {
-  var path=''
-  if (typeof req.query.id !== 'undefined' && req.query.id !== null) {
-    path=req.query.id;
-  }
+	var path=''
+	if (typeof req.query.id !== 'undefined' && req.query.id !== null) {
+		path=req.query.id;
+	}
 
-  return path;
+	return path;
 }
 
 app.use(express.static(path.join(__dirname, 'public')))
 const folder = 'C:/Users/User/Downloads'
 
 app.get('/', function(req, res) {
-  logUser(req);
-  res.sendFile(path.join(__dirname + '/index.htm'))
+	logUser(req);
+	res.sendFile(path.join(__dirname + '/index.htm'));
 })
 
 app.get('/api', function(req, res) {
-  logUser(req);
+	logUser(req);
 
-  var path = getPath(req);
-
-  jsonRespone = helper.getDirListJSON(folder, path);
-  res.contentType('application/json');
-  res.send(JSON.stringify(jsonRespone));
+	var path = getPath(req);
+	jsonRespone = helper.getDirListJSON(folder, path);
+	res.contentType('application/json');
+	res.send(JSON.stringify(jsonRespone));
 })
 
 app.get('/no-js', function(req, res) {
-  logUser(req);
+	logUser(req);
 
-  var path=getPath(req);
-
-  var out = helper.getDirListHTML(folder, path);
-  res.send(out);
+	var path=getPath(req);
+	var out = helper.getDirListHTML(folder, path);
+	res.send(out);
 })
 
 app.get('/video', function(req, res) {
-  logUser(req);
-  helper.loadVideo(folder, req, res);
+	logUser(req);
+	helper.loadVideo(folder, req, res);
 })
 
 app.listen(3000, function () {
-  console.log('Listening on port 3000!')
+	console.log('Listening on port 3000!')
 })
