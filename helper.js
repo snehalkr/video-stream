23ed032ec59a89b982459c81f57f0b7550a4d88a
snehalkr <30932507+snehalkr@users.noreply.github.com>
2019-03-11 01:06:16 +0530

    Added Back Button and some organization

--- helper.js
+++ helper.js
@@ -3,11 +3,7 @@ const fs = require('fs')
 const path = require('path')
 const app = express()
 
-exports.sendErrorMessage = function() {
-  return ;
-}
-
-exports.getDirList = function(folder, path) {
+function getDirList(folder, path) {
   var arr=[];
   fullPath = folder + '' + path;
 
@@ -29,4 +25,67 @@ exports.getDirList = function(folder, path) {
   return arr;
 }
 
+exports.getDirListJSON = function(folder, path) {
+  result = getDirList(folder, path);
+  curDir = folder + '' + path;
+  jsonRespone = {cur:path,files:result};
+  return jsonRespone;
+}
+
+
+exports.getDirListHTML = function(folder, path) {
+  var out = '';
+
+  result = getDirList(folder, path);
+  result.forEach(function(curItem) {
+    link = path + '/' + curItem.link;
+    if(curItem.type==0)
+      out+='<a href="/no-js?id='+link+'">' + curItem.link + '</a> : Folder</br>\n';
+    else
+      out+='<a href="/video?id='+link+'">' + curItem.link + '</a> : File</br>\n';
+  })
+  return out;
+}
+
+exports.loadVideo = function(folder, req, res) {
+  var path = folder+''+req.query.id;
+
+  if (!fs.existsSync(path)) {
+    res.send('Video Not Found!');
+    return ;
+  }
+
+  const stat = fs.statSync(path)
+  const fileSize = stat.size
+  const range = req.headers.range
+
+  var id = req.query.id;
+
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = parts[1]
+      ? parseInt(parts[1], 10)
+      : fileSize-1
+
+    const chunksize = (end-start)+1
+    const file = fs.createReadStream(path, {start, end})
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    }
+
+    res.writeHead(206, head)
+    file.pipe(res)
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(path).pipe(res)
+  }
+}
 // console.log(this.getDirList('C:/unity/nodejs/video-stream-sample'));
--- server.js
+++ server.js
@@ -8,6 +8,15 @@ function logUser(req) {
   console.log(req.connection.remoteAddress);
 }
 
+function getPath(req) {
+  var path=''
+  if (typeof req.query.id !== 'undefined' && req.query.id !== null) {
+    path=req.query.id;
+  }
+
+  return path;
+}
+
 app.use(express.static(path.join(__dirname, 'public')))
 const folder = 'C:/Users/User/Downloads'
 
@@ -19,13 +28,9 @@ app.get('/', function(req, res) {
 app.get('/api', function(req, res) {
   logUser(req);
 
-  var path=''
-  if (typeof req.query.id !== 'undefined' && req.query.id !== null) {
-    path=req.query.id;
-  }
-  result = helper.getDirList(folder, path);
-  curDir = folder + '' + path;
-  jsonRespone = {cur:path,files:result}
+  var path = getPath(req);
+
+  jsonRespone = helper.getDirListJSON(folder, path);
   res.contentType('application/json');
   res.send(JSON.stringify(jsonRespone));
 })
@@ -33,65 +38,15 @@ app.get('/api', function(req, res) {
 app.get('/no-js', function(req, res) {
   logUser(req);
 
-  var path=''
-  if (typeof req.query.id !== 'undefined' && req.query.id !== null) {
-    path=req.query.id;
-  }
-  result = helper.getDirList(folder, path);
-  
-  var out = '';
-  result.forEach(function(curItem) {
-    link = path + '/' + curItem.link;
-    if(curItem.type==0)
-      out+='<a href="/no-js?id='+link+'">' + curItem.link + '</a> : Folder</br>\n';
-    else
-      out+='<a href="/video?id='+link+'">' + curItem.link + '</a> : File</br>\n';
-  })
+  var path=getPath(req);
 
+  var out = helper.getDirListHTML(folder, path);
   res.send(out);
 })
 
 app.get('/video', function(req, res) {
   logUser(req);
-  var path = folder+''+req.query.id;
-
-  if (!fs.existsSync(path)) {
-    res.send('Video Not Found!');
-    return ;
-  }
-
-  const stat = fs.statSync(path)
-  const fileSize = stat.size
-  const range = req.headers.range
-
-  var id = req.query.id;
-
-  if (range) {
-    const parts = range.replace(/bytes=/, "").split("-")
-    const start = parseInt(parts[0], 10)
-    const end = parts[1]
-      ? parseInt(parts[1], 10)
-      : fileSize-1
-
-    const chunksize = (end-start)+1
-    const file = fs.createReadStream(path, {start, end})
-    const head = {
-      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': 'video/mp4',
-    }
-
-    res.writeHead(206, head)
-    file.pipe(res)
-  } else {
-    const head = {
-      'Content-Length': fileSize,
-      'Content-Type': 'video/mp4',
-    }
-    res.writeHead(200, head)
-    fs.createReadStream(path).pipe(res)
-  }
+  helper.loadVideo(folder, req, res);
 })
 
 app.listen(3000, function () {
